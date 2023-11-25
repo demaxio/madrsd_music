821255d8926a9f2a7e61c4e8cbc664f9733a0402
demaxio <45129571+demaxio@users.noreply.github.com>
2023-11-25 20:10:09 +0400

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -10,372 +10,372 @@
 //paste it inside the array as more as you want music then you don't need to do any other thing
 
 let allMusic = [
-{
-  name: "5th Element",
-  album: "Electronic Boom (2016)",
-  img: "ElectronicBoom",
-  src: "Fifth-Element"
-},
-{
-  name: "Everything Now",
-  album: "Happy Feelings (2016)",
-  img: "HappyFeelings",
-  src: "Everything-Now"
-},
-{
-  name: "Long Way to Paradise",
-  album: "Happy Feelings (2016)",
-  img: "HappyFeelings",
-  src: "Long-Way-to-Paradise"
-},
-{
-  name: "Out of problems",
-  album: "Happy Feelings (2016)",
-  img: "HappyFeelings",
-  src: "Out-of-problems"
-},
-{
-  name: "Times I",
-  album: "Times (2016)",
-  img: "Times",
-  src: "Times-I"
-},
-{
-  name: "Abyssal chime I",
-  album: "Abyssal Chimes (2017)",
-  img: "AbyssalChimes",
-  src: "Abyssal-chime-I"
-},
-{
-  name: "Abyssal chime II",
-  album: "Abyssal Chimes (2017)",
-  img: "AbyssalChimes",
-  src: "Abyssal-chime-II"
-},
-{
-  name: "The last bastion",
-  album: "Fallen Angel (2017)",
-  img: "FallenAngel",
-  src: "The-last-bastion"
-},
-{
-  name: "Ancient Anarchy",
-  album: "Historical Ritmix (2017)",
-  img: "HistoricalRitmix",
-  src: "Ancient-Anarchy"
-},
-{
-  name: "After us",
-  album: "Happy Feelings II (2017)",
-  img: "HappyFeelingsII",
-  src: "After-us"
-},
-{
-  name: "The spring yard",
-  album: "Happy Feelings II (2017)",
-  img: "HappyFeelingsII",
-  src: "The-spring-yard"
-},
-{
-  name: "Slow Move",
-  album: "Technotronic (2017)",
-  img: "Technotronic",
-  src: "Slow-Move-I-II"
-},
-{
-  name: "Game Bit",
-  album: "Game Bit (2017)",
-  img: "GameBit",
-  src: "Game-Bit"
-},
-{
-  name: "Mega Game",
-  album: "Game Bit (2017)",
-  img: "GameBit",
-  src: "Mega-Game"
-},
-{
-  name: "Betrayer",
-  album: "Singles II (2017)",
-  img: "Betrayer",
-  src: "Betrayer"
-},
-{
-  name: "Forgotten Sands",
-  album: "Singles II (2017)",
-  img: "ForgottenSands",
-  src: "Forgotten-Sands"
-},
-{
-  name: "Secret Window",
-  album: "Singles II (2017)",
-  img: "SecretWindow",
-  src: "Secret-window"
-},
-{
-  name: "The land of Conflict (Remix by Evil Dad)",
-  album: "Singles III (2017)",
-  img: "TLoC",
-  src: "The-land-of-Conflict"
-},
-{
-  name: "Lost",
-  album: "Singles III (2017)",
-  img: "Lost",
-  src: "Lost"
-},
-{
-  name: "Depression",
-  album: "Emotions (2018)",
-  img: "Emotions",
-  src: "Depression"
-},
-{
-  name: "The Road of Distress",
-  album: "Others (2018)",
-  img: "TheRoadofDistress",
-  src: "The-road-of-Distress"
-},
-{
-  name: "Signs III",
-  album: "Signs (2018)",
-  img: "SIGNS",
-  src: "Signs-III"
-},
-{
-  name: "Holidays",
-  album: "Singles IV (2018)",
-  img: "Holidays",
-  src: "Holidays"
-},
-{
-  name: "Strange Night",
-  album: "Singles IV (2018)",
-  img: "StrangeNight",
-  src: "Strange-Night"
-},
-{
-  name: "Dark Dreams",
-  album: "Sad Feelings (2019)",
-  img: "SadFeelings",
-  src: "Dark-Dreams"
-},
-{
-  name: "Lost Heaven",
-  album: "Sad Feelings (2019)",
-  img: "SadFeelings",
-  src: "Lost-Heaven"
-},
-{
-  name: "Phase V",
-  album: "Phases (2019)",
-  img: "Phases",
-  src: "Phase-V"
-},
-{
-  name: "Noise",
-  album: "Singles V (2019)",
-  img: "Noise",
-  src: "Noise"
-},
-{
-    name: "The last ship",
-    album: "Singles VI (2019)",
-    img: "ThelastShip",
-    src: "The-last-ship"
+    {
+        name: "5th Element",
+        album: "Electronic Boom (2016)",
+        img: "ElectronicBoom",
+        src: "Fifth-Element",
+    },
+    {
+        name: "Everything Now",
+        album: "Happy Feelings (2016)",
+        img: "HappyFeelings",
+        src: "Everything-Now",
+    },
+    {
+        name: "Out of problems",
+        album: "Happy Feelings (2016)",
+        img: "HappyFeelings",
+        src: "Out-of-problems",
+    },
+    {
+        name: "Times I",
+        album: "Times (2016)",
+        img: "Times",
+        src: "Times-I",
+    },
+    {
+        name: "Abyssal chime I",
+        album: "Abyssal Chimes (2017)",
+        img: "AbyssalChimes",
+        src: "Abyssal-chime-I",
+    },
+    {
+        name: "Abyssal chime II",
+        album: "Abyssal Chimes (2017)",
+        img: "AbyssalChimes",
+        src: "Abyssal-chime-II",
+    },
+    {
+        name: "The last bastion",
+        album: "Fallen Angel (2017)",
+        img: "FallenAngel",
+        src: "The-last-bastion",
+    },
+    {
+        name: "Ancient Anarchy",
+        album: "Historical Ritmix (2017)",
+        img: "HistoricalRitmix",
+        src: "Ancient-Anarchy",
+    },
+    {
+        name: "After us",
+        album: "Happy Feelings II (2017)",
+        img: "HappyFeelingsII",
+        src: "After-us",
+    },
+    {
+        name: "The spring yard",
+        album: "Happy Feelings II (2017)",
+        img: "HappyFeelingsII",
+        src: "The-spring-yard",
+    },
+    {
+        name: "Slow Move",
+        album: "Technotronic (2017)",
+        img: "Technotronic",
+        src: "Slow-Move-I-II",
+    },
+    {
+        name: "Game Bit",
+        album: "Game Bit (2017)",
+        img: "GameBit",
+        src: "Game-Bit",
+    },
+    {
+        name: "Mega Game",
+        album: "Game Bit (2017)",
+        img: "GameBit",
+        src: "Mega-Game",
+    },
+    {
+        name: "Betrayer",
+        album: "Singles II (2017)",
+        img: "Betrayer",
+        src: "Betrayer",
+    },
+    {
+        name: "Forgotten Sands",
+        album: "Singles II (2017)",
+        img: "ForgottenSands",
+        src: "Forgotten-Sands",
+    },
+    {
+        name: "Secret Window",
+        album: "Singles II (2017)",
+        img: "SecretWindow",
+        src: "Secret-window",
+    },
+    {
+        name: "The land of Conflict (Remix by Evil Dad)",
+        album: "Singles III (2017)",
+        img: "TLoC",
+        src: "The-land-of-Conflict",
+    },
+    {
+        name: "Depression",
+        album: "Emotions (2018)",
+        img: "Emotions",
+        src: "Depression",
+    },
+    {
+        name: "The Road of Distress",
+        album: "Others (2018)",
+        img: "TheRoadofDistress",
+        src: "The-road-of-Distress",
+    },
+    {
+        name: "Signs III",
+        album: "Signs (2018)",
+        img: "SIGNS",
+        src: "Signs-III",
+    },
+    {
+        name: "Holidays",
+        album: "Singles IV (2018)",
+        img: "Holidays",
+        src: "Holidays",
+    },
+    {
+        name: "Strange Night",
+        album: "Singles IV (2018)",
+        img: "StrangeNight",
+        src: "Strange-Night",
+    },
+    {
+        name: "Dark Dreams",
+        album: "Sad Feelings (2019)",
+        img: "SadFeelings",
+        src: "Dark-Dreams",
+    },
+    {
+        name: "Lost Heaven",
+        album: "Sad Feelings (2019)",
+        img: "SadFeelings",
+        src: "Lost-Heaven",
+    },
+    {
+        name: "Phase V",
+        album: "Phases (2019)",
+        img: "Phases",
+        src: "Phase-V",
+    },
+    {
+        name: "Noise",
+        album: "Singles V (2019)",
+        img: "Noise",
+        src: "Noise",
+    },
+    {
+        name: "The last ship",
+        album: "Singles VI (2019)",
+        img: "ThelastShip",
+        src: "The-last-ship",
+    },
+    {
+        name: "Lonely Knight",
+        album: "Singles VI (2019)",
+        img: "LonelyKnight",
+        src: "Lonely-knight",
+    },
+    {
+        name: "The lost city",
+        album: "Singles VI (2019)",
+        img: "TheLostCity",
+        src: "The-lost-city",
+    },
+    {
+        name: "Abyssal Chimes IX",
+        album: "Abyssal Chimes II (2020)",
+        img: "AbyssalChimesII",
+        src: "Abyssal-Chimes-IX",
+    },
+    {
+        name: "Purgatory",
+        album: "Hell's Sounds (2020)",
+        img: "HellsSounds",
+        src: "Purgatory",
+    },
+    {
+        name: "Retrolize",
+        album: "Singles VII (2020)",
+        img: "Retrolize",
+        src: "Retrolize",
+    },
+    {
+        name: "The rising light",
+        album: "Singles VII (2020)",
+        img: "therisinglight",
+        src: "the-rising-light",
+    },
+    {
+        name: "Heart Noise",
+        album: "Singles VII (2020)",
+        img: "HeartNoise",
+        src: "Heart-Noise",
+    },
+    {
+        name: "First flight to Mars (Remix by EVIL DAD)",
+        album: "Rest on Mars (2021)",
+        img: "RestonMars",
+        src: "First-flight-to-Mars",
+    },
+    {
+        name: "Mars",
+        album: "Rest on Mars (2021)",
+        img: "RestonMars",
+        src: "Mars",
+    },
+    {
+        name: "Endless Morning",
+        album: "Dissolved in silence (2021)",
+        img: "DissolvedInSilence",
+        src: "Endless-Morning",
+    },
+    {
+        name: "Autumn Day",
+        album: "Dissolved in silence (2021)",
+        img: "DissolvedInSilence",
+        src: "Autumn-Day",
+    },
+    {
+        name: "Calm Evening",
+        album: "Dissolved in silence (2021)",
+        img: "DissolvedInSilence",
+        src: "Calm-Evening",
+    },
+
+    {
+        name: "Abyssal Chimes VII (Remix)",
+        album: "Singles VIII (2021)",
+        img: "AbyssalChimesIIREMIX",
+        src: "Abyssal-Chimes-VII",
+    },
+    {
+        name: "Trainventure (Remastered Edition)",
+        album: "Singles VIII (2021)",
+        img: "trainventure",
+        src: "Trainventure",
+    },
+    {
+        name: "Безымянка",
+        album: "Samara Metro (2022)",
+        img: "SamaraMetro",
+        src: "Безымянка",
+    },
+    {
+        name: "Советская",
+        album: "Samara Metro (2022)",
+        img: "SamaraMetro",
+        src: "Советская",
+    },
+    {
+        name: "Гагаринская",
+        album: "Samara Metro (2022)",
+        img: "SamaraMetro",
+        src: "Гагаринская",
+    },
+    {
+        name: "Российская",
+        album: "Samara Metro (2022)",
+        img: "SamaraMetro",
+        src: "Российская",
+    },
+    {
+        name: "Lost in Clouds",
+        album: "Singles IX (2022)",
+        img: "Lost-in-clouds",
+        src: "Lost-in-the-clouds",
+    },
+    {
+        name: "New Detroit",
+        album: "Dystopia 2049 (2022)",
+        img: "Dystopia",
+        src: "New-Detroit",
+    },
+    {
+        name: "Dystopia 2049",
+        album: "Dystopia 2049 (2022)",
+        img: "Dystopia",
+        src: "Dystopia-2049",
+    },
+    {
+        name: "Dystopia 2048",
+        album: "Dystopia 2049 (2022)",
+        img: "Dystopia",
+        src: "Dystopia-2048",
+    },
+    {
+        name: "Restless Night",
+        album: "Dystopia 2049 (2022)",
+        img: "Dystopia",
+        src: "Restless-Night",
+    },
+    {
+        name: "The Thing (True Version)",
+        album: "Singles (2022)",
+        img: "TheThing",
+        src: "The-Thing",
+    },
+    {
+        name: "Abyssal chime XIII",
+        album: "Abyssal Chimes III (2022)",
+        img: "AbyssalChimesIII",
+        src: "Abyssal-Chimes-XIII",
+    },
+    {
+        name: "Abyssal chime XV",
+        album: "Abyssal Chimes III (2022)",
+        img: "AbyssalChimesIII",
+        src: "Abyssal-Chimes-XV",
+    },
+    {
+        name: "Beyond",
+        album: "Singles X (2022)",
+        img: "Beyond",
+        src: "Beyond",
+    },
+    {
+        name: "Soft Serenity",
+        album: "Happy Feelings III (2023)",
+        img: "HappyFeelingsIII",
+        src: "Soft-Serenity",
+    },
+    {
+      name: "Peaceful Place",
+      album: "Happy Feelings III (2023)",
+      img: "HappyFeelingsIII",
+      src: "Peaceful-Place",
   },
-  {
-    name: "Lonely Knight",
-    album: "Singles VI (2019)",
-    img: "LonelyKnight",
-    src: "Lonely-knight"
+    {
+        name: "Летняя ночь",
+        album: "Панельный рассвет (2023)",
+        img: "Панельныйрассвет",
+        src: "Летняя-ночь",
+    },
+    {
+        name: "Lost (Remix)",
+        album: "Technotronic II (2023)",
+        img: "TechnotronicII",
+        src: "Lost",
+    },
+    {
+        name: "Technotronic II",
+        album: "Technotronic II (2023)",
+        img: "TechnotronicII",
+        src: "Technotronic-II",
+    },
+    {
+      name: "Restless Night II",
+      album: "Singles X (2023)",
+      img: "Restless-Night-II",
+      src: "Restless-Night-II",
   },
-  {
-    name: "The lost city",
-    album: "Singles VI (2019)",
-    img: "TheLostCity",
-    src: "The-lost-city"
-  },
-  {
-    name: "Abyssal Chimes IX",
-    album: "Abyssal Chimes II (2020)",
-    img: "AbyssalChimesII",
-    src: "Abyssal-Chimes-IX"
-  },
-  {
-    name: "Purgatory",
-    album: "Hell's Sounds (2020)",
-    img: "HellsSounds",
-    src: "Purgatory"
-  },
-  {
-    name: "Retrolize",
-    album: "Singles VII (2020)",
-    img: "Retrolize",
-    src: "Retrolize"
-  },
-  {
-    name: "The rising light",
-    album: "Singles VII (2020)",
-    img: "therisinglight",
-    src: "the-rising-light"
-  },
-  {
-    name: "Heart Noise",
-    album: "Singles VII (2020)",
-    img: "HeartNoise",
-    src: "Heart-Noise"
-  },
-  {
-    name: "First flight to Mars (Remix by EVIL DAD)",
-    album: "Rest on Mars (2021)",
-    img: "RestonMars",
-    src: "First-flight-to-Mars"
-  },
-  {
-    name: "Mars",
-    album: "Rest on Mars (2021)",
-    img: "RestonMars",
-    src: "Mars"
-  },
-  {
-    name: "Endless Morning",
-    album: "Dissolved in silence (2021)",
-    img: "DissolvedInSilence",
-    src: "Endless-Morning"
-  },
-  {
-    name: "Autumn Day",
-    album: "Dissolved in silence (2021)",
-    img: "DissolvedInSilence",
-    src: "Autumn-Day"
-  },
-  {
-    name: "Calm Evening",
-    album: "Dissolved in silence (2021)",
-    img: "DissolvedInSilence",
-    src: "Calm-Evening"
-  },
-  
-  {
-    name: "Abyssal Chimes VII (Remix)",
-    album: "Singles VIII (2021)",
-    img: "AbyssalChimesIIREMIX",
-    src: "Abyssal-Chimes-VII"
-  },
-  {
-    name: "Trainventure (Remastered Edition)",
-    album: "Singles VIII (2021)",
-    img: "trainventure",
-    src: "Trainventure"
-  },
-  {
-    name: "Юнгородок",
-    album: "Samara Metro (2022)",
-    img: "SamaraMetro",
-    src: "Юнгородок"
-  },
-  {
-    name: "Безымянка",
-    album: "Samara Metro (2022)",
-    img: "SamaraMetro",
-    src: "Безымянка"
-  },
-  {
-    name: "Советская",
-    album: "Samara Metro (2022)",
-    img: "SamaraMetro",
-    src: "Советская"
-  },
-  {
-    name: "Гагаринская",
-    album: "Samara Metro (2022)",
-    img: "SamaraMetro",
-    src: "Гагаринская"
-  },
-  {
-    name: "Российская",
-    album: "Samara Metro (2022)",
-    img: "SamaraMetro",
-    src: "Российская"
-  },
-  {
-    name: "Lost in Clouds",
-    album: "Singles IX (2022)",
-    img: "Lost-in-clouds",
-    src: "Lost-in-the-clouds"
-  },
-  {
-    name: "Requiem",
-    album: "Singles IX (2022)",
-    img: "Requiem",
-    src: "Requiem"
-  },
-  {
-    name: "Neverland",
-    album: "Singles IX (2022)",
-    img: "Neverland",
-    src: "Neverland"
-  },
-  {
-  name: "Dead Planet II",
-  album: "Singles IX (2022)",
-  img: "dead-planet-II",
-  src: "Dead-Planet-II"
-  },
-  {
-  name: "New Detroit",
-  album: "Dystopia 2049 (2022)",
-  img: "Dystopia",
-  src: "New-Detroit"
-  },
-  {
-  name: "Dystopia 2049",
-  album: "Dystopia 2049 (2022)",
-  img: "Dystopia",
-  src: "Dystopia-2049"
-  },
-  {
-  name: "Dystopia 2048",
-  album: "Dystopia 2049 (2022)",
-  img: "Dystopia",
-  src: "Dystopia-2048"
-  },
-  {
-  name: "Restless Night",
-  album: "Dystopia 2049 (2022)",
-  img: "Dystopia",
-  src: "Restless-Night"
-  },
-  {
-  name: "The Thing (True Version)",
-  album: "Singles (2022)",
-  img: "TheThing",
-  src: "The-Thing"
-  },
-  {
-  name: "Abyssal chime XIII",
-  album: "Abyssal Chimes III (2022)",
-  img: "AbyssalChimesIII",
-  src: "Abyssal-Chimes-XIII"
-  },
-  {
-  name: "Abyssal chime XV",
-  album: "Abyssal Chimes III (2022)",
-  img: "AbyssalChimesIII",
-  src: "Abyssal-Chimes-XV"
-  },
-  {
-  name: "Beyond",
-  album: "Singles X (2022)",
-  img: "Beyond",
-  src: "Beyond"
-  },
-  // like this paste it and remember to give comma after ending of this bracket }
-  // {
-  //   name: "Here is the music name",
-  //   album: "Here is the album name",
-  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-  // }
-  ];
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   album: "Here is the album name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
